c82f8ad70f320f4bbe8221545f96307bc41f6bdc
ANIRUDH YADAV <anirudh.yadav.ug23@nsut.ac.in>
2026-05-14 11:44:29 +0530

    Simplify MongoDB client initialization
    Removed environment variable loading for MongoDB configuration.

--- src/db/mongo_client.py
+++ src/db/mongo_client.py
@@ -2,21 +2,10 @@
 MongoDB client initialization.
 """
 
-import os
-
-from dotenv import load_dotenv
 from motor.motor_asyncio import AsyncIOMotorClient
 
-load_dotenv()
+MONGO_URL = "mongodb://localhost:27017"
+DB_NAME = "adaptive_rag"
 
-MONGO_URL = os.getenv("MONGODB_URL", "mongodb://localhost:27017")
-DB_NAME = os.getenv("MONGODB_DB_NAME", "adaptive_rag")
-SERVER_SELECTION_TIMEOUT_MS = int(
-	os.getenv("MONGODB_SERVER_SELECTION_TIMEOUT_MS", "3000")
-)
-
-client = AsyncIOMotorClient(
-	MONGO_URL,
-	serverSelectionTimeoutMS=SERVER_SELECTION_TIMEOUT_MS,
-)
+client = AsyncIOMotorClient(MONGO_URL)
 db = client[DB_NAME]
